776fb92f71ea3b9f47478e086d634dcaafebdbc9
hulloanson <hulloanson@gmail.com>
2020-08-28 11:37:25 +0800

    got mojim results

--- index.src.js
+++ index.src.js
@@ -1,5 +1,20 @@
 const fetch = require('node-fetch')
+const { parse } = require('node-html-parser')
+
+const parseResult = (rawHtml) => {
+  const root = parse(rawHtml)
+  console.log(root.)
+}
 
 const search = (text) => {
-  const urlEncoded = 
+  const uriEncoded = encodeURI(text)
+  const searchUrl = `http://mojim.com/${uriEncoded}.html`
+  fetch(searchUrl)
+    .then(res => res.text())
+    .then(parseResult)
+    .catch(err => {
+      console.log('======err=======\n', err)
+    })
 }
+
+search()
