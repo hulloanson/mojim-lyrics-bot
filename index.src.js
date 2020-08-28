b8b8b71f24913e0e883ef55d2199525580c547e4
hulloanson <hulloanson@gmail.com>
2020-08-29 02:30:59 +0800

    fixed getting link. used cheerio

--- index.src.js
+++ index.src.js
@@ -1,48 +1,22 @@
 const fetch = require('node-fetch')
-// const { parse, } = require('node-html-parser')
 const cheerio = require('cheerio')
 const TurndownService = require('turndown')
 
-const parseLinkFromResultNode = (node) => {
-  const lyricsPointer = node.querySelector('.mxsh_ss3')
-  const linkElem = lyricsPointer && lyricsPointer.querySelector('a')
-  console.log('============link elem =================')
-  console.log(linkElem)
-  const link = linkElem && linkElem.getAttribute('href')
-  console.log('============link=================')
-  console.log(link)
-  const id = link && parseId(link)
-  const songLink = id && genSongLink(id)
-  return songLink
-}
-
-const findFirstResult = (resultContainer) => {
-  return resultContainer.childNodes &&
-    resultContainer.childNodes.find(n =>
-      n.classNames && n.classNames.find(
-        cn => cn.match(/mxsh_dd(1|2)/))
-    )
-}
-
 const getFirstLinkFromResults = (rawHtml) => {
   const $ = cheerio.load(rawHtml)
-  const oddRes = $('.mxsh_dd1')
-  const evenRes = $('.mxsh_dd2')
-  const len = Math.max(oddRes.length, evenRes.length)
-  let res = []
-  for (let i = 0; i < len; i++) {
-    res[2*i] = oddRes[i]
-    res[2*i + 1] = evenRes[i]
-  }
+  const res = $('.mxsh_ss3 > a')
+  console.log('======res=====')
   console.log(res)
-  throw new Error('exit')
-  console.log('==============oddRes======================')
-  console.log(Array.from(oddRes))
-  console.log('==============evenRes======================')
-  console.log(Array.from(evenRes))
-  return parseLinkFromResultNode(firstRes)
-  // if (!childNodes || childNodes.length === 0) return null
-  // return parseResult(childNodes[0])
+  const uri = res.length > 0 && res[0] && res[0].attribs && res[0].attribs['href']
+  console.log('======uri=====')
+  console.log(uri)
+  const id = uri && parseId(uri)
+  console.log('======id=====')
+  console.log(id)
+  const link = id && genSongLink(id)
+  console.log('======link=====')
+  console.log(link)
+  return link
 }
 
 const parseLyrics = (rawHtml) => {
@@ -59,7 +33,7 @@ const removeEmptyLines = (str) => {
 }
 
 const parseId = (href) => {
-  const matches = Array.from(href.matchAll(/[0-9]{6}x[0-9]{2}x[0-9]/))
+  const matches = Array.from(href.matchAll(/[0-9]+x[0-9]+x[0-9]+/))
   return matches && matches[0] && matches[0][0]
 }
 
@@ -86,4 +60,4 @@ const search = (text) => {
     })
 }
 
-search('晚期拖延症患者')
+search('想你的夜')
