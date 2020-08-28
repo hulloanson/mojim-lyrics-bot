c1e9fd050a110a875308d7c235c5eac5a785e9e2
hulloanson <hulloanson@gmail.com>
2020-08-28 12:11:35 +0800

    got lyrics link

--- index.src.js
+++ index.src.js
@@ -3,8 +3,32 @@ const { parse } = require('node-html-parser')
 
 const parseResult = (rawHtml) => {
   const root = parse(rawHtml)
-  console.log(root.)
+  const resultContainer = root.querySelector('.mxsh_dl0')
+  const childNodes = resultContainer.childNodes
+  for (const n of childNodes) {
+    if (n.classNames
+      && n.classNames.some(t => t.match(/^mxsh_dd(1|2)$/))
+    ) {
+      const lyricsPointer = n.querySelector('.mxsh_ss3')
+      const linkElem = lyricsPointer && lyricsPointer.querySelector('a')
+      console.log('============link elem =================')
+      console.log(linkElem)
+      const link = linkElem && linkElem.getAttribute('href')
+      console.log('============link=================')
+      console.log(link)
+      const id = parseId(link)
+      const songLink = genSongLink(id)
+      console.log(songLink)
+      break
+    }
+  }
 }
+
+const parseId = (href) => {
+  return Array.from(href.matchAll(/[0-9]{6}x[0-9]{2}x[0-9]/))[0][0]
+}
+
+const genSongLink = (id) => `http://mojim.com/twy${id}.htm`
 
 const search = (text) => {
   const uriEncoded = encodeURI(text)
@@ -17,4 +41,4 @@ const search = (text) => {
     })
 }
 
-search()
+search("缺口 庾澄慶")
